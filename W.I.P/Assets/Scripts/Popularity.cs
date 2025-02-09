0e4c34323c140eb02ba43abefc8376d9ff8563a3
AlexH-LBS <alexander.holmstedt@elev.ga.lbs.se>
2025-02-10 00:24:24 +0100

    final tweaks

--- W.I.P/Assets/Scripts/Popularity.cs
+++ W.I.P/Assets/Scripts/Popularity.cs
@@ -8,6 +8,12 @@ public class Popularity : MonoBehaviour
     public float crowdSpeed = 0.0f;
     public float increase = 1.01f;
 
+    public GameObject crowdLayer1;
+    public GameObject crowdLayer2;
+    public GameObject crowdLayer3;
+    public GameObject crowdLayer4;
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -15,13 +21,38 @@ public class Popularity : MonoBehaviour
         {
             crowdSpeed = 0;
         }
+        crowdLayer1.SetActive(false);
+        crowdLayer2.SetActive(false);
+        crowdLayer3.SetActive(false);
+        crowdLayer4.SetActive(false);
 
     }
 
     // Update is called once per frame
     void Update()
     {
-       
+
+        if (fameInt > 10)
+        {
+            crowdLayer1.SetActive(true);
+
+            if (fameInt > 30)
+            {
+                crowdLayer2.SetActive(true);
+
+                if (fameInt > 50)
+                {
+                    crowdLayer3.SetActive(true);
+
+                    if (fameInt > 90)
+                    {
+                        crowdLayer4.SetActive(true);
+                    }
+                }
+            }
+        }
+
+
 
     }
 
